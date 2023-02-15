4350ae75f60baada1a4e1b4fc51fa0a98d1dc719
Jonas <jonas.sajonas@gmail.com>
2023-02-16 09:39:14 +1000

    initial upload

---
+++ repeatstrings.js
@@ -0,0 +1,10 @@
+let string = 'hey';
+const repeatString = function () {
+    for (let num = 0; num <= 3; num++) {
+         string * num;
+    }
+    console.log (repeatString);
+};
+
+repeatString();
+console.log (repeatString);
